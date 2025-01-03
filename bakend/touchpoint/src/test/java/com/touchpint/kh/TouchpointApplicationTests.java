87858b092026f8f37095287fe8f2254d5ed60bf6
hantaeyeok <gksxodur1@gmail.com>
2025-01-03 16:40:55 +0900

    project setting

---
+++ bakend/touchpoint/src/test/java/com/touchpint/kh/TouchpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touchpint.kh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouchpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
