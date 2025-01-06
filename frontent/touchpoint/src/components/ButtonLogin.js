909dfed03e7a54c674060057c27ff708addd8293
hantaeyeok <gksxodur1@gmail.com>
2025-01-06 16:15:28 +0900

    11

---
+++ frontent/touchpoint/src/components/ButtonLogin.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/ButtonLogin.css"
+
+
+const ButtonLogin = ({ children, onClick, type = "button", className = "", url }) => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+      if (url) {        // React Router를 사용한 URL 이동
+        navigate(url);
+      } else if (onClick) {// 사용자 정의 onClick 함수 실행
+        onClick();
+      }
+    };
+  
+    return (
+      <button
+        className={`button ${className}`}
+        type={type}
+        onClick={handleClick}
+      >
+        {children}
+      </button>
+    );
+  };
+  
+  export default ButtonLogin;
