b3c7f64af697d9e9f7934753d1c0bc8956b654e3
jongwooleezzang <whddn1310@naver.com>
2025-01-08 20:23:05 +0900

    lee 0108 커밋
    리액트 부트 연동

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,33 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'; // axios를 사용해 Spring Boot API 호출
 import './App.css';
 import Menubar from './components/indexcomponents/Menubar';
 
-
-
 function App() {
+  const [data, setData] = useState(""); // Spring Boot 데이터 상태 관리
+
+  useEffect(() => {
+    // Spring Boot API 호출
+    axios
+      .get("http://localhost:8989/api/test")
+      .then((response) => {
+        console.log("Response from server:", response.data);
+        setData(response.data); // API 응답 데이터를 상태로 저장
+      })
+      .catch((error) => {
+        console.error("Error fetching data:"  , error);
+      });
+  }, []);
+
   return (
     <div className="index">
-      <div ClassName="Menubar">
-       <Menubar></Menubar>
+      <div className="Menubar">
+        <Menubar />
       </div>
-      
+     
+      <div className="content">
+        <h1>{data}</h1>
+      </div>
     </div>
   );
 }
