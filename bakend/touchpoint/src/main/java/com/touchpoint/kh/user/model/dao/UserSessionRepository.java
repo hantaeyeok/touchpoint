41bad15283b54e6004bec3cfad55d63c92461b7b
hantaeyeok <gksxodur1@gmail.com>
2025-01-13 16:00:55 +0900

    commit
    commit

--- bakend/touchpoint/src/main/java/com/touchpoint/kh/user/model/dao/UserSessionRepository.java
+++ bakend/touchpoint/src/main/java/com/touchpoint/kh/user/model/dao/UserSessionRepository.java
@@ -9,7 +9,6 @@ import com.touchpoint.kh.user.model.vo.UserSession;
 
 @Repository
 public interface UserSessionRepository extends JpaRepository<UserSession, Long> {
-
     // 특정 사용자 ID로 활성화된 세션 검색
     Optional<UserSession> findByUserIdAndSessionStatus(String userId, String sessionStatus);
 }
