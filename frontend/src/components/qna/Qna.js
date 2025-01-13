0d72888dcf9b5d1d9d5d299f45bde2774530ac15
hwan9207 <160584145+hwan9207@users.noreply.github.com>
2025-01-13 14:32:04 +0900

    commit
    commit

--- frontend/src/components/qna/Qna.js
+++ frontend/src/components/qna/Qna.js
@@ -14,7 +14,51 @@ function Qna() {
                 <Link to="/qna">질문하기</Link>
                 <Link to="/faq">자주 묻는 질문</Link>
             </div>
-                <h3>질문하기 화면</h3>
+            <div className="search-container">
+                <input
+                type="text"
+                placeholder="검색어를 입력해주세요."
+                value={''}
+                onChange={''}
+                />
+                <button>검색</button>
+            </div>
+            <div>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>NO.</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>등록일</th>
+                        <th>상태</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>3.</td>
+                        <td>제목 제목 제목 제목 제목</td>
+                        <td>게스트</td>
+                        <td>25/2/8</td>
+                        <td class="status question">질문중</td>
+                    </tr>
+                    <tr>
+                        <td>3.</td>
+                        <td>제목 제목 제목 제목 제목</td>
+                        <td>게스트</td>
+                        <td>25/2/8</td>
+                        <td class="status answered">답변완료</td>
+                    </tr>
+                    <tr>
+                        <td>3.</td>
+                        <td>제목 제목 제목 제목 제목</td>
+                        <td>게스트</td>
+                        <td>25/2/8</td>
+                        <td class="status question">질문중</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
