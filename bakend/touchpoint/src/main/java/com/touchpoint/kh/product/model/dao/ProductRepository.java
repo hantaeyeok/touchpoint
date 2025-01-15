7c06f988396d638a1ff75a6b101f0490144c39b9
jongwooleezzang <whddn1310@naver.com>
2025-01-15 11:43:16 +0900

    LEE_0115
    커밋

--- bakend/touchpoint/src/main/java/com/touchpoint/kh/product/model/dao/ProductRepository.java
+++ bakend/touchpoint/src/main/java/com/touchpoint/kh/product/model/dao/ProductRepository.java
@@ -3,10 +3,12 @@ package com.touchpoint.kh.product.model.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.touchpoint.kh.product.model.vo.Product;
 
-public interface ProductRepository extends JpaRepository<Product, String>{
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
 
-	List<Product> findByProductCategory(String category);
+    List<Product> findByProductCategory(String category);
 }
--- bakend/touchpoint/src/main/java/com/touchpoint/kh/product/model/service/ProductServiceImpl.java
+++ bakend/touchpoint/src/main/java/com/touchpoint/kh/product/model/service/ProductServiceImpl.java
@@ -11,21 +11,20 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
-	private final ProductRepository productRepository;
-	
-	@Override
-	public Product save(Product product) {
-		return productRepository.save(product);
-	}
-	
-	@Override
-	public List<Product> findAll() {
-		return productRepository.findAll();
-	}
-	
-	@Override
-	public List<Product> findByProductCategory(String category){
-		return productRepository.findByProductCategory(category);
-	}
-}
+    private final ProductRepository productRepository;
 
+    @Override
+    public Product save(Product responseProduct) {
+        return productRepository.save(responseProduct);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> findByProductCategory(String category){
+        return productRepository.findByProductCategory(category);
+    }
+}
