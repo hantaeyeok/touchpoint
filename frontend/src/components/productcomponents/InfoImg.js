0a134aada9990d1216938b090f5ddaa716493a34
김민아 <110553201+Kimmingle@users.noreply.github.com>
2025-01-29 04:00:54 +0900

    className 충돌 수정

--- frontend/src/components/productcomponents/InfoImg.js
+++ frontend/src/components/productcomponents/InfoImg.js
@@ -14,9 +14,9 @@ function InfoImg() {
             <div className="text-overlay">
                 <p className="service-name">TouchPoint Service</p>
                 <h1 className="main-text">매장에 필요한 모든것, TouchPoint</h1>
-                <div className="page-indicator">
-                    <div className="page-num" >
-                        <span className="current-page">1</span> / <span className="total-page">5</span>
+                <div className="page-buttonPro">
+                    <div className="page-numPro" >
+                        <span className="current-pagePro">1</span> / <span className="total-pagePro">5</span>
                     </div>
                 </div>
             </div>
